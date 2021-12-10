5bb14249926b1652aea6ff7308631a9d71317bf3
eebann <ebanemmanuel@live.com>
2021-12-10 06:53:40 -0600

    defining array

---
+++ arrays.js
@@ -0,0 +1,24 @@
+//arrays : ordered, changeable, indexed, can contain multiple data types
+console.log('***********welcome to arrays in javascript************');
+let colors = ["red", "blue", "green"];
+console.log(colors);
+
+let data =[1,3,'man','woman']
+console.log(data)
+
+// changing a property in an array
+
+let shoppingList	= ['cheddar cheese','milk' ] 
+
+shoppingList[1] = 'whole milk' //not the index are counted from 0
+
+console.log(shoppingList)
+
+// what if the data of the shopping list is unknown and you want to add a data to the end of the list
+// in this case we can use lenth property
+
+shoppingList[shoppingList.length] = 'bread' //adding a new item to the array end
+shoppingList[shoppingList.length] = 'butter' //adding a new item to the array end
+console.log(shoppingList) //note the length property is the number of items in the array
+
+
