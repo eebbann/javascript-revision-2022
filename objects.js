a12bdd29988fea0127bd5e3d4171b21cbf2e1440
eebann <ebanemmanuel@live.com>
2021-12-13 09:25:12 -0600

    adding properties to objects

--- objects.js
+++ objects.js
@@ -29,3 +29,37 @@ console.log(fitBitData[45]);//to access a non string value
 		'300g' : 'three hundred',
 	}
 	console.log(numb['300g']);
+
+	//when to use quotes in objects
+	//- when you have a key that has a space in it
+	//- when you have a key that has a special character in it
+	//- when you have a key that has a number in it
+
+	const myObj = {
+		'My dog': 'Fido',
+		'My cat': 'Fluffy',
+		'My hamster': 'Harold',
+		3: 'three',
+	}
+	console.log(myObj['My dog']);
+	console.log(myObj['My cat']);
+	console.log(myObj['My hamster']);
+	console.log(myObj[3]);
+
+	 
+ //adding properties to objects
+	const myOb = {};
+	myOb.name = 'John';
+	myOb.age = '27';
+	myOb.hobby = 'coding';
+	myOb.isMarried = 'no';
+	console.log(myOb);
+
+	//adding properties to objects
+	const myOb = {};
+	myOb['name'] = 'John';
+	myOb['age'] = '27';
+	myOb['hobby'] = 'coding';	
+	myOb['isMarried'] = 'no';
+	console.log(myOb);
+ 
