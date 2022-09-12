7951fb74520d822433c8ba69953ea80b6c949754
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2022-09-12 13:48:06 -0500

    push

---
+++ functions/callApplyBind.js
@@ -0,0 +1,6 @@
+const array1 = ['#b', "6o", "y88"];
+
+const found = array1.find(element => element == "b");
+
+console.log(found);
+// expected output: 12
--- functions/es6.js
+++ functions/es6.js
@@ -14,4 +14,24 @@ let number2 = numbers.forEach((item,index)=> console.log(item, index))
 // with two parameters for arrow functions use brackets notation
 
 //TIPS AND GOTCHAS
+ const f1 = (param) => param ; // note this returns a given argument even without the return 
+ console.log(f1(09))
 
+ const f2 = (param)=> {   param} ;  
+ console.log(f2('er')) // this will run undefine because using curly brackets you need return keyword
+
+ //using underscore when theres no arguments
+ const vim = 89
+ const f4 = _ => vim ; // underscore replaces  empty params can use () instead
+ console.log(f4())
+
+ // returning objects in arrow functions
+
+const profile={
+	"id":09,
+	"name":'jum',
+	"age":32,
+}
+
+const func = (prof)=> ({prof}) //notice for returning objects in arrow functions you and brsckets and curly brackets
+console.log(func(profile))
