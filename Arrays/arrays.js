619b900047a9f41f24845095ed695e01b2d5f402
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2022-10-03 12:12:01 -0500

    array starting

---
+++ Arrays/arrays.js
@@ -0,0 +1,6 @@
+//arrays in javascript is the collection that of alll the data types which is accessible by a variable name
+
+var collection = ['minecraft', 'item', 'inventory', 'inventoryItem', 'inventoryItem']
+
+console.log(collection.length ) // length of the collection is ppropety types
+
--- functions/closures.js
+++ functions/closures.js
@@ -4,8 +4,27 @@ function outer(){
 	function inner(){
 		console.log(b + 8)
 	}
- return inner;
+ return inner; // returns a fuction 
 }
 
 outer()
  
+
+//closures in callbacks
+var a = 10
+
+var fn = function(){
+	// console.log(a + 8)
+}
+setTimeout(fn, 3000)
+
+for(var i = 0; i <= 9; i++) {
+		setTimeout(function() {
+			console.log(i )
+		},i * 2000)
+}
+
+
+///
+var i;
+var print = function (){}
