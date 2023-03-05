16bfdec0953ea9d9c39b72a8928da0fad454f511
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2023-03-05 05:41:28 -0600

    array

---
+++ revision/varscope.js
@@ -0,0 +1,11 @@
+// console.log(mb);
+// console.log(vanum);
+// var vanum = 0
+
+//array
+let ma= new Array() 
+ma.length = 12;
+ma.fill(8)
+console.log(ma)
+
+//
--- variableandscopes/varscope.js
+++
@@ -1,4 +0,0 @@
-// console.log(mb);
-console.log(vanum);
-var vanum = 0
-let mb = 8``
