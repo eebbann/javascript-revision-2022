6868a62d3e3f70e4ef166ddad91abd1ba8dceb8b
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2023-04-06 20:01:17 -0500

    added drag event

--- DOM/connectinhHtml/script.js
+++ DOM/connectinhHtml/script.js
@@ -33,3 +33,23 @@ for(let i = 0; i< 3; i++){
 }
 parent.append(...listItems)
 // /
+
+const button = document.getElementById('button')
+
+const scroll = document.getElementById('scrollable')
+
+button.addEventListener('dragstart', (e) => {
+	console.log(e)
+	button.style.background="green";
+	button.style.color="white"
+})
+
+scroll.addEventListener('drop', () =>{
+scroll.prepend(button)
+scroll.style.backgroundColor ="red";
+scroll.style.padding = "10px"
+})
+
+scroll.addEventListener('dragover',(e)=>{
+	e.preventDefault()
+})
