adc90e673078266108ac00ebda082aa655eeef38
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2023-04-20 17:43:44 -0500

    starting javascript interview test

---
+++ Javascript test/main.js
@@ -0,0 +1,13 @@
+//function declaration
+// when you state a function declaration
+
+//function Expression
+const started = function(num){
+	return num * num
+}
+
+started()
+
+
+//first class Functions ?
+
