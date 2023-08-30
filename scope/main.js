501d00676442531d4e233acbafc4285b6177b550
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2023-08-30 08:12:51 -0500

    declaration initialization reassignment

---
+++ scope/main.js
@@ -0,0 +1,22 @@
+ var global = "pop global"
+
+  function go() {
+		let m = "pop"
+		var n = "lolo"
+		return m + n;
+ }
+if (true){
+	let b = "pikl"
+}
+console.log(b)
+
+// redeclaration and ressignment
+
+let g = "book"
+// let g = "pop"  cannot be redeclared only with var
+
+g = "pinterest"
+
+//initialization 
+// const af; - initialization doesnt work with this
+let boy;
--- z15-javascriptProject/Accordion/app.js
+++ z15-javascriptProject/Accordion/app.js
@@ -1,9 +1,23 @@
-const btns = document.querySelectorAll('.question-btn')
-// const question  = document.querySelector('.question-text')
- console.log("p")
-btns.forEach(function(btn){
-btn.addEventListener("click", function(e){
-const parent =e.currentTarget.parentElement.parentElement;
-parent.classList.toggle("show-text")
+//using selectors
+const questions = document.querySelectorAll('.question');
+questions.forEach(o=>{
+const btn = o.querySelector('.question-btn')
+ btn.addEventListener("click", function(){
+	
+ })
 })
-}) 
+
+
+
+
+
+//traversing the dom 
+// const btns = document.querySelectorAll('.question-btn')
+// // const question  = document.querySelector('.question-text')
+//  console.log("p")
+// btns.forEach(function(btn){
+// btn.addEventListener("click", function(e){
+// const parent =e.currentTarget.parentElement.parentElement;
+// parent.classList.toggle("show-text")
+// })
+// }) 
